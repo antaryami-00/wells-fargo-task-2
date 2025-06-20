265a58d91a12b83a4b63a9bbf392b81f65e85b8a
antaryami-00 <antaryami.sahu.707@gmail.com>
2025-06-20 19:53:25 +0530

    Update

--- src/main/java/com/wellsfargo/counselor/repo/ClientRepository.java
+++ src/main/java/com/wellsfargo/counselor/repo/ClientRepository.java
@@ -1,1 +1,7 @@
+package com.wellsfargo.counselor.repo;
+
+import com.yourproject.entity.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientRepository extends JpaRepository<Client, Long> {}
 
