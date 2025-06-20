521eed892cb623908c971196eaa2f319ced24ba0
antaryami-00 <antaryami.sahu.707@gmail.com>
2025-06-20 19:20:47 +0530

    Update Client.java

--- src/main/java/com/wellsfargo/counselor/entity/Client.java
+++ src/main/java/com/wellsfargo/counselor/entity/Client.java
@@ -1,1 +1,50 @@
+package com.yourproject.entities;
+
+import javax.persistence.*;
+
+@Entity
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String phone;
+
+    @ManyToOne
+    @JoinColumn(name = "advisor_id")
+    private FinancialAdvisor advisor;
+
+    @OneToOne(mappedBy = "client", cascade = CascadeType.ALL)
+    private Portfolio portfolio;
+
+    public Client() {}
+
+    public Client(String name, String email, String phone, FinancialAdvisor advisor) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.advisor = advisor;
+    }
+
+    // Getters and Setters
+    public Long getId() { return id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPhone() { return phone; }
+    public void setPhone(String phone) { this.phone = phone; }
+
+    public FinancialAdvisor getAdvisor() { return advisor; }
+    public void setAdvisor(FinancialAdvisor advisor) { this.advisor = advisor; }
+
+    public Portfolio getPortfolio() { return portfolio; }
+    public void setPortfolio(Portfolio portfolio) { this.portfolio = portfolio; }
+}
 
