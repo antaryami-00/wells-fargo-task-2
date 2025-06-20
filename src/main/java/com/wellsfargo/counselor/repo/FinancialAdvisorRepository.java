030335720d335901a10986c5e1ec3406287e0f7b
antaryami-00 <antaryami.sahu.707@gmail.com>
2025-06-20 19:52:11 +0530

    Update

--- src/main/java/com/wellsfargo/counselor/repo/FinancialAdvisorRepository.java
+++ src/main/java/com/wellsfargo/counselor/repo/FinancialAdvisorRepository.java
@@ -1,1 +1,7 @@
+package com.wellsfargo.counselor.repo;
+
+import com.yourproject.entity.FinancialAdvisor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FinancialAdvisorRepository extends JpaRepository<FinancialAdvisor, Long> {}
 
