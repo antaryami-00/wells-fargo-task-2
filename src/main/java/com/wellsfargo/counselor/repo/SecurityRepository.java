af4e2a865b4f585549bc1956fb349cfda7805839
antaryami-00 <antaryami.sahu.707@gmail.com>
2025-06-20 19:55:24 +0530

    Update

--- src/main/java/com/wellsfargo/counselor/repo/SecurityRepository.java
+++ src/main/java/com/wellsfargo/counselor/repo/SecurityRepository.java
@@ -1,1 +1,7 @@
+package com.wellsfargo.counselor.repo;
+
+import com.yourproject.entity.Security;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SecurityRepository extends JpaRepository<Security, Long> {}
 
