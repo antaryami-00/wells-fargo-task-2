0bc2bd6159c6f32400d76deac7e145d2840cc600
antaryami-00 <antaryami.sahu.707@gmail.com>
2025-06-20 19:54:16 +0530

    Update

--- src/main/java/com/wellsfargo/counselor/repo/PortfolioRepository.java
+++ src/main/java/com/wellsfargo/counselor/repo/PortfolioRepository.java
@@ -1,1 +1,7 @@
+package com.wellsfargo.counselor.repo;
+
+import com.yourproject.entity.Portfolio;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PortfolioRepository extends JpaRepository<Portfolio, Long> {}
 
